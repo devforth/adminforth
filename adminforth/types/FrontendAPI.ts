18d02c54f00d04e5c0bba3494d0d19db680ab93e
IhorStorozhok <storozhokigor@gmail.com>
2024-06-14 13:34:29 +0300

    added documetnation

--- adminforth/types/FrontendAPI.ts
+++ adminforth/types/FrontendAPI.ts
@@ -8,12 +8,28 @@ export interface FrontendAPIInterface {
     /**
      * Show a confirmation dialog
      * 
+     * The dialog will be displayed to the user
+     * Example: 
+     * 
+     * ```ts
+     * const isConfirmed = await window.adminforth.confirm({message: 'Are you sure?', yes: 'Yes', no: 'No'})
+     * if (isConfirmed) {
+     *  your code...
+     * }
+     * ```
+     * 
      * @param params - The parameters of the dialog
      * @returns A promise that resolves when the user confirms the dialog
      */   
     confirm(params:ConfirmParams ): Promise<void>;
     /**
      * Show an alert
+     * 
+     * The alert will be displayed to the user
+     * 
+     * ```ts
+     * Example: window.adminforth.alert({message: 'Hello', variant: 'success'})
+     * ```
      * 
      * @param params - The parameters of the alert
      */
