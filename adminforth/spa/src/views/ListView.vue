3aaf20314717cd05a6b9581fb28a738154691fc6
IhorStorozhok <storozhokigor@gmail.com>
2024-06-24 15:52:38 +0300

    bulk fix

--- adminforth/spa/src/views/ListView.vue
+++ adminforth/spa/src/views/ListView.vue
@@ -70,7 +70,7 @@
           class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400"
           v-if="filtersStore.filters.length">
             {{ filtersStore.filters.length }}
-          </span>
+        </span>
       </button>
     </BreadcrumbsWithButtons>
 
