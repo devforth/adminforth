6541e975b321ff537ee6121c884da858bce3f655
IhorStorozhok <storozhokigor@gmail.com>
2024-06-25 17:36:37 +0300

    added skelet loader

---
+++ adminforth/spa/src/components/SkeleteLoader.vue
@@ -0,0 +1,23 @@
+<template>
+    <tr v-for="c in new Array(props.columns)" class="bg-lightListTable border-b dark:bg-darkListTable dark:border-darkListBorder">
+         <td v-for="r in new Array(props.rows)" class="items-center px-6 py-20 cursor-default" >
+            <div role="status" class="max-w-sm animate-pulse">
+                <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            </div>
+        </td>
+    </tr>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    columns:Number,
+    rows:Number
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
