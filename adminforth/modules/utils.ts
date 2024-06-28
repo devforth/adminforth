1fda0bd1c263b70f20fad5b36c90324b80ba7008
IhorStorozhok <storozhokigor@gmail.com>
2024-06-28 17:12:31 +0300

    added ts ignore

--- adminforth/modules/utils.ts
+++ adminforth/modules/utils.ts
@@ -1,11 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+// @ts-ignore-next-line
 import csscolors from 'css-color-names';
-
-
-
-
 
 
 export function guessLabelFromName(name) {
