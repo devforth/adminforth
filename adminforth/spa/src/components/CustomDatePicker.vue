91bce1d98c470ee4f7a90fee8c39ffec116e91ac
Pavlo Salata <psalata@devforth.io>
2024-07-24 21:43:55 +0300

    fix typo

--- adminforth/spa/src/components/CustomDatePicker.vue
+++ adminforth/spa/src/components/CustomDatePicker.vue
@@ -11,7 +11,7 @@
 
           <input ref="datepickerStartEl" type="text"
                  class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                 placeholder="Start date">
+                 placeholder="Select date">
         </div>
       </div>
     </div>
