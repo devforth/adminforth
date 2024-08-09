8b980957b8639a5e4df41e16d604f8443c9e86f3
Ivan Borshchov <ivan@devforth.io>
2024-08-09 13:26:40 +0300

    add demoCredentials and loginPromptHTML

--- adminforth/spa/src/views/LoginView.vue
+++ adminforth/spa/src/views/LoginView.vue
@@ -61,6 +61,17 @@
                           </div>
                         </div>
                         
+
+                        <div v-if="coreStore.config?.loginPromptHTML"
+                          class="flex items-center p-4 mb-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-400" role="alert"
+                        >
+                          <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                          </svg>
+                          <span class="sr-only">Info</span>
+                          <div v-html="coreStore.config?.loginPromptHTML"></div>
+                        </div>
+
                         <button 
                           @click="login"
                           type="submit" class="flex items-center justify-center gap-1 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -106,8 +117,14 @@ const showPw = ref(false);
 
 const error = ref(null);
 
-onMounted(() => {
-    coreStore.getPublicConfig();
+onMounted(async () => {
+    await coreStore.getPublicConfig();
+    if (coreStore.config?.demoCredentials) {
+      console.log('Setting demo credentials');
+      const [username, password] = coreStore.config.demoCredentials.split(':');
+      usernameInput.value.value = username;
+      passwordInput.value.value = password;
+    }
     usernameInput.value.focus();
 });
 
--- demo/index.ts
+++ demo/index.ts
@@ -96,6 +96,8 @@ const admin = new AdminForth({
     passwordHashField: 'password_hash',
     userFullNameField: 'fullName', // optional
     loginBackgroundImage: 'https://images.unsplash.com/photo-1499988921418-b7df40ff03f9?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    demoCredentials: "adminforth:adminforth",  // never use it for production
+    loginPromptHTML: "Use email <b>adminforth</b> and password <b>adminforth</b> to login",
     // loginBackgroundImage: '@@/pho.jpg',
   },
   customization: {
