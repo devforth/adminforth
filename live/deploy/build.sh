64b11f27a343ae1285062dfe31bb575b5f764169
Ivan Borshchov <ivan@devforth.io>
2024-08-09 15:58:51 +0300

    add harbor

--- live/deploy/build.sh
+++ live/deploy/build.sh
@@ -3,6 +3,16 @@ set -e
 HOST_DOMAIN=3.127.41.171
 mkdir -p ~/.docker/$HOST_DOMAIN
 
+
+
+HARBOR_USERNAME='robot$adminforth-demo+wp'
+HARBOR_REGISTRY_BASE_URL=harbor.devforth.io/adminforth-demo+wp/
+docker login -u $HARBOR_USERNAME -p $VAULT_HARBOR_KEY $HARBOR_REGISTRY_BASE_URL
+
+docker compose -p stack-af-live -f compose.yml build
+docker compose -p stack-af-live -f compose.yml push
+
+
 echo "$VAULT_MAIN_CA_PEM_KEY" > ~/.docker/$HOST_DOMAIN/ca.pem
 echo "$VAULT_MAIN_KEY_PEM_KEY" > ~/.docker/$HOST_DOMAIN/key.pem
 echo "$VAULT_MAIN_CERT_PEM_KEY" > ~/.docker/$HOST_DOMAIN/cert.pem
@@ -14,5 +24,7 @@ export DOCKER_CERT_PATH=~/.docker/$HOST_DOMAIN
 docker builder prune -a -f
 docker container prune -f
 
-docker compose -p stack-af-live -f compose.yml up -d --build --remove-orphans
 
+
+
+docker compose -p stack-af-live -f compose.yml  up -d --pull always --remove-orphans --wait
