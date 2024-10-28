94074672d5cc0b4adba469a4ee6495747798b800
Ivan Borshchov <ivan@devforth.io>
2024-10-28 15:13:12 +0200

    fix https://web.tracklify.com/project/2b7ZVgE5/AdminForth/261/pQqQUxKU/dark-triangle

--- adminforth/spa/src/components/ResourceListTable.vue
+++ adminforth/spa/src/components/ResourceListTable.vue
@@ -32,9 +32,9 @@
                 class="flex items-center " :class="{'cursor-pointer':c.sortable}">
               {{ c.label }}
 
-              <div v-if="c.sortable"
-                  :style="{ 'color':ascArr.includes(c.name)?'green':descArr.includes(c.name)?'red':'currentColor'}">
+              <div v-if="c.sortable">
                 <svg v-if="ascArr.includes(c.name) || descArr.includes(c.name)" class="w-3 h-3 ms-1.5"
+                    fill='currentColor'
                     :class="{'rotate-180':descArr.includes(c.name)}" viewBox="0 0 24 24">
                   <path
                     d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847"/>
