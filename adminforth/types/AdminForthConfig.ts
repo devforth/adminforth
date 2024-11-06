aee7478358df1f527698b2a98e228b1ca325b2ea
Ivan Borshchov <ivan@devforth.io>
2024-11-06 12:51:17 +0200

    add docs

--- adminforth/types/AdminForthConfig.ts
+++ adminforth/types/AdminForthConfig.ts
@@ -556,6 +556,14 @@ export type AdminForthResourceColumn = {
      * An optional configuration object for extra settings.
      */
     extra?: {
+
+      /**
+       * How many levels of JSON should be collapsed. 
+       * 0 means - root level will be already collapsed e.g. {a:1} will show {...} where '...' is clickable
+       * 1 means - root level will be shown, but next sub-level will be collapsed e.g. {a: {b: 1}} will show {a: ...} where '...' is clickable
+       * 
+       * Default is 1. 
+       */
       jsonCollapsedLevel?: number
     }
 
