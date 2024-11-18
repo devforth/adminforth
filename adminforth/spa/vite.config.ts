d3af4e8f20b85ab7b8eab9caeb2c446227412d73
Ivan Borshchov <ivan@devforth.io>
2024-11-18 11:58:16 +0200

    fix vite hmr port clashes

--- adminforth/spa/vite.config.ts
+++ adminforth/spa/vite.config.ts
@@ -2,35 +2,32 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import portfinder from 'portfinder';
 
+/**
+ * Find the next available port after a specified port.
+ * @param {number} startPort - The starting port to check.
+ * @returns {Promise<number>} - A promise that resolves with the next available port.
+ */
+async function getNextAvailablePort(startPort) {
+  return await portfinder.getPortPromise({ port: startPort });
+};
 
+const appPort = await getNextAvailablePort(5173);
+const hmrPort = await getNextAvailablePort(5273);
+console.log(`App port: ${appPort}. HMR port: ${hmrPort}`);
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.VITE_ADMINFORTH_PUBLIC_PATH || '/',
   server: {
-    port: 5173,
+    port: appPort,
     hmr: {
       path: '/adminforth-dev-server-ws', // Set your custom WebSocket path here
-      port: 5273,
+      port: hmrPort,
     },
   },
   plugins: [
     vue(),
-    // {
-    //   name: 'hmr-dynamic-port-plugin',
-    //   configureServer(server) {
-    //     server.httpServer?.once('listening', () => {
-    //       const actualPort = server.config.server.port;
-    //       if (server.ws) {
-    //         if (!server.ws.options) {
-    //           server.ws.options = {};
-    //         }
-    //         server.ws.options.port = actualPort; // Update the WebSocket port
-    //       }
-    //       console.log(`Server is running on port ${actualPort}`);
-    //     });
-    //   },
-    // },
   ],
   resolve: {
     alias: {
