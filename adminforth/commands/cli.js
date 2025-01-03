af149dd2ff5202ec11fc176f6c05b8ce82c83f6f
Viacheslav Mamchurovskyi <slavuta1212@gmail.com>
2025-01-03 13:00:56 +0200

    add `create-app` CLI command

--- adminforth/commands/cli.js
+++ adminforth/commands/cli.js
@@ -3,10 +3,14 @@
 const args = process.argv.slice(2);
 const command = args[0];
 
-import generateModels from "./generateModels.js";
 import bundle from "./bundle.js";
+import createApp from "./createApp/main.js";
+import generateModels from "./generateModels.js";
 
 switch (command) {
+  case "create-app":
+    createApp(args);
+    break;
   case "generate-models":
     generateModels();
     break;
@@ -14,5 +18,5 @@ switch (command) {
     bundle();
     break;
   default:
-    console.log("Unknown command. Available commands: generate-models, bundle");
+    console.log("Unknown command. Available commands: create-app, generate-models, bundle");
 }
