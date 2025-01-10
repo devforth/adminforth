b6d365e57c9f5508c8e41c4d41275b9b162f9018
Ivan Borshchov <ivan@devforth.io>
2025-01-10 12:32:23 +0200

    fix dev demo

---
+++ plugins/install-plugins.sh
@@ -0,0 +1,22 @@
+PLUGINS="adminforth-audit-log adminforth-email-password-reset adminforth-foreign-inline-list \
+adminforth-i18n adminforth-import-export adminforth-text-complete adminforth-open-signup \
+adminforth-rich-editor adminforth-two-factors-auth adminforth-upload"
+
+# for each plugin
+for plugin in $PLUGINS; do
+
+  if [ -d "$plugin/.git" ]; then
+      echo "Repository for $plugin exists. Pulling latest changes..."
+      cd "$plugin"
+      git pull
+  else
+      echo "Repository for $plugin does not exist. Cloning..."
+      git clone git@github.com:devforth/$plugin.git
+      cd $plugin
+  fi
+ 
+  npm ci
+  cd ..
+done
+
+
