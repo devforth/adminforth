4b214af8c0b3a96f8a6210215d277aa9ab0827cc
Ivan Borshchov <ivan@devforth.io>
2025-01-13 20:33:58 +0200

    fix(build): broken npm run build detection

--- live-demo/deploy/buildRelease.sh
+++ live-demo/deploy/buildRelease.sh
@@ -3,10 +3,11 @@
 
 # write npm run output both to console and to build.log
 npm run build 2>&1 | tee build.log
+build_status=${PIPESTATUS[0]}
 
 # if exist status from the npm run build is not 0
 # then exit with the status code from the npm run build
-if [ ${PIPESTATUS[0]} -ne 0 ]; then
-  echo "Build failed. Exiting with status code ${PIPESTATUS[0]}"
-  exit ${PIPESTATUS[0]}
+if [ $build_status -ne 0 ]; then
+  echo "Build failed. Exiting with status code $build_status"
+  exit $build_status
 fi
