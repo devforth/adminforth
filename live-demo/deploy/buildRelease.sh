e9e4223409208c65315ee4ea7f129eeaaf3a17e9
Ivan Borshchov <ivan@devforth.io>
2025-01-13 11:08:40 +0200

    move build to buildRelease.sh

---
+++ live-demo/deploy/buildRelease.sh
@@ -0,0 +1,9 @@
+
+# write npm run output both to console and to build.log
+npm run build 2>&1 | tee build.log; 
+
+# if exist status from the npm run build is not 0
+# then exit with the status code from the npm run build
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+  exit ${PIPESTATUS[0]}
+fi
