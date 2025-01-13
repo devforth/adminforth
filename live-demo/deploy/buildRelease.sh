40dbdc5c2f2b737055a500a764c96688ce7b594a
Ivan Borshchov <ivan@devforth.io>
2025-01-13 15:46:51 +0200

    try tee in all commands

--- live-demo/deploy/buildRelease.sh
+++ live-demo/deploy/buildRelease.sh
@@ -4,6 +4,10 @@
 # write npm run output both to console and to build.log
 npm run build 2>&1 | tee build.log
 
+npm audit signatures | tee build.log
+
+npx semantic-release | tee build.log
+
 # if exist status from the npm run build is not 0
 # then exit with the status code from the npm run build
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
