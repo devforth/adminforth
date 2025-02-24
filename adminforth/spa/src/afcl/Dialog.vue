08e0a1679284822a77f22f60c87fc2aefc27a69c
Petr Kachanovsky <pkachanovsky@devforth.io>
2025-02-24 12:19:38 +0200

    fix: change nested dialog render point to prevent it being behind darkened background

--- adminforth/spa/src/afcl/Dialog.vue
+++ adminforth/spa/src/afcl/Dialog.vue
@@ -5,52 +5,54 @@
   >
     <slot name="trigger"></slot>
   </div>
-  <div ref="modalEl" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div v-bind="$attrs" class="relative p-4 max-w-2xl max-h-full" :class="$attrs.class?.includes('w-') ? '' : 'w-full'">
-      <!-- Modal content -->
-      <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-        <!-- Modal header -->
-        <div
-          v-if="header"
-          class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200"
-        >
-          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            {{ header }}
-          </h3>
-          <button
-            v-if="headerCloseButton"
-            type="button"
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            @click="modal?.hide()"
+  <Teleport to="body">
+    <div ref="modalEl" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div v-bind="$attrs" class="relative p-4 max-w-2xl max-h-full" :class="$attrs.class?.includes('w-') ? '' : 'w-full'">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+          <!-- Modal header -->
+          <div
+            v-if="header"
+            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200"
           >
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-            </svg>
-            <span class="sr-only">Close modal</span>
-          </button>
-        </div>
-        <!-- Modal body -->
-        <div class="p-4 md:p-5 space-y-4 text-gray-700 dark:text-gray-400">
-          <slot></slot>
-        </div>
-        <!-- Modal footer -->
-        <div
-          v-if="buttons.length"
-          class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
-        >
-          <Button
-            v-for="(button, buttonIndex) in buttons"
-            :key="buttonIndex"
-            v-bind="button.options"
-            :class="{ 'ms-3': buttonIndex > 0 }"
-            @click="button.onclick(modal)"
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              {{ header }}
+            </h3>
+            <button
+              v-if="headerCloseButton"
+              type="button"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              @click="modal?.hide()"
+            >
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <div class="p-4 md:p-5 space-y-4 text-gray-700 dark:text-gray-400">
+            <slot></slot>
+          </div>
+          <!-- Modal footer -->
+          <div
+            v-if="buttons.length"
+            class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
           >
-            {{ button.label }}
-          </Button>
+            <Button
+              v-for="(button, buttonIndex) in buttons"
+              :key="buttonIndex"
+              v-bind="button.options"
+              :class="{ 'ms-3': buttonIndex > 0 }"
+              @click="button.onclick(modal)"
+            >
+              {{ button.label }}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
