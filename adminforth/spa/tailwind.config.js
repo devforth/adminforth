8dfc690a559b8e2d0fa194660bec2aa8b07b81fa
Ivan Borshchov <ivan@devforth.io>
2025-05-19 20:33:47 +0000

    fix: return tooltips on apexcharts after flowbite update - we use standalopne apexcharts now so don't need flowbite components on it, they seem to conflict

--- adminforth/spa/tailwind.config.js
+++ adminforth/spa/tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-import flowbitePlugin from 'flowbite/plugin';
 
 export default {
   content:  ["./src/**/*.{vue, js, ts, tsx}","./src/*.{vue, js, ts, tsx}", "./index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       /* IMPORTANT:ADMINFORTH TAILWIND STYLES */
-  }
+    }
   },
 
   darkMode: 'class',
   plugins: [
-    flowbitePlugin({
-      charts: true,
-    }),
+
   ],
 }
 
