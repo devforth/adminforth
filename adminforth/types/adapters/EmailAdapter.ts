60efb7f6297ec48de7964a9e3938e0318dd1c289
yaroslav8765 <ypechorkin@devforth.io>
2025-07-14 12:42:13 +0300

    fix: removed Adapters.ts and created 5 new adapters files and one index.js file

---
+++ adminforth/types/adapters/EmailAdapter.ts
@@ -0,0 +1,27 @@
+export interface EmailAdapter {
+
+  /**
+   * This method is called to validate the configuration of the adapter
+   * and should throw a clear user-readbale error if the configuration is invalid.
+   */
+  validate(): Promise<void>;
+
+  /**
+   * This method should send an email using the adapter
+   * @param from - The sender's email address
+   * @param to - The recipient's email address
+   * @param text - The plain text version of the email
+   * @param html - The HTML version of the email
+   * @param subject - The subject of the email
+   */
+  sendEmail(
+    from: string,
+    to: string,
+    text: string,
+    html: string,
+    subject: string
+  ): Promise<{
+    error?: string;
+    ok?: boolean;
+  }>;
+}
---
+++ adminforth/types/adapters/index.js
@@ -0,0 +1,5 @@
+export { default as EmailAdapter } from './EmailAdapter.ts';
+export { default as CompletionAdapter } from './CompletionAdapter.ts';
+export { default as ImageGenerationAdapter } from './ImageGenerationAdapter.ts';
+export { default as OAuth2Adapter } from './OAuth2Adapter.ts';
+export { default as StorageAdapter } from './StorageAdapter.ts';
