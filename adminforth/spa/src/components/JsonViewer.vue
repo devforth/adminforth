43921cf91390bad47922bd34571a97cc6e87620a
yaroslav8765 <ypechorkin@devforth.io>
2025-07-21 16:36:24 +0300

    feat: Add JsonViewer component

---
+++ adminforth/spa/src/components/JsonViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <JsonViewer
+    class="afcl-json-viewer min-w-[6rem]"
+    :value="value"
+    :expandDepth="expandDepth"
+    copyable
+    sort
+  />
+</template>
+
+<script setup lang="ts">
+import { JsonViewer } from 'vue3-json-viewer'
+
+defineProps<{
+  value: any
+  expandDepth?: number
+}>()
+
+</script>
