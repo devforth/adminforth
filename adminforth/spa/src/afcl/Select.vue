75fb7f9f2cfec64a962c2dc737c87ed6de46f081
yaroslav8765 <ypechorkin@devforth.io>
2025-07-21 14:03:43 +0300

    fix: release

--- adminforth/spa/src/afcl/Select.vue
+++ adminforth/spa/src/afcl/Select.vue
@@ -74,7 +74,6 @@
       <div v-if="!filteredItems.length" class="px-4 py-2 cursor-pointer text-gray-400 dark:text-gray-300">
         {{ options.length ? $t('No results found') : $t('No items here') }}
       </div>
-
       <div v-if="$slots['extra-item']"  class="px-4 py-2 dark:text-gray-400">
         <slot name="extra-item"></slot>
       </div>
