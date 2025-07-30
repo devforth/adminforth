c5630a795fc694170fe3b19c1979c1a59231ccec
yaroslav8765 <ypechorkin@devforth.io>
2025-07-30 12:59:21 +0300

    chore: use nextTick insted of setTimeout in Select.vue

--- adminforth/spa/src/afcl/Select.vue
+++ adminforth/spa/src/afcl/Select.vue
@@ -110,7 +110,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, onUnmounted, watch, type Ref } from 'vue';
+import { ref, computed, onMounted, onUnmounted, watch, nextTick, type Ref } from 'vue';
 import { IconCaretDownSolid } from '@iconify-prerendered/vue-flowbite';
 import { useElementSize } from '@vueuse/core'
 
@@ -175,10 +175,7 @@ function updateFromProps() {
   }
 }
 
-function inputClick() {
-  setTimeout(() => {
-    handleScroll();
-  }, 0);
+async function inputClick() {
 
   if (props.readonly) return;
   // Toggle local dropdown
@@ -187,6 +184,9 @@ function inputClick() {
   if (!showDropdown.value && !search.value) {
     search.value = '';
   }
+
+  await nextTick();
+  handleScroll();
 }
 
 watch(
