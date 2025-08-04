6d697d043704f21db8d4ebe45eeeeb1f6c21732a
yaroslav8765 <pechorkin2014@gmail.com>
2025-08-04 15:52:01 +0300

    chore: create test page with all afcl components

--- dev-demo/custom/Dash.vue
+++ dev-demo/custom/Dash.vue
@@ -1,32 +1,231 @@
 <template>
 
-  
+  <div class="flex gap-10">
+    <div class="flex flex-col max-w-[200px] m-10 mt-20 gap-10">
+      <Checkbox :disabled="false"><p>afdsdfsdfsdgsdgsgdsggdg</p> </Checkbox>
+      <Button @click="doSmth" 
+        :loader="false" class="w-full">
+        Your button text
+      </Button>
 
-  <div class="m-20">
-    <Checkbox :disabled="false"><p>afdsdfsdfsdgsdgsgdsggdg</p> </Checkbox>
+      <Button @click="doSmth" 
+          :loader="true" class="w-full mt-4">
+        Your button text
+      </Button>
+
+      <Link to="/login">Go to login</Link>
+
+      <LinkButton to="/login">Go to login</LinkButton>
+
+      <Select
+        class="w-full"
+        :options="[
+          {label: 'Last 7 days', value: '7'}, 
+          {label: 'Last 30 days', value: '30'}, 
+          {label: 'Last 90 days', value: '90'},
+          {label: 'None', value: null}
+        ]"
+        v-model="selected"
+      ></Select>
+
+      <Select
+        class="w-full"
+        :options="[
+          {label: 'Last 7 days', value: '7'}, 
+          {label: 'Last 30 days', value: '30'}, 
+          {label: 'Last 90 days', value: '90'},
+          {label: 'None', value: null}
+        ]"
+        v-model="selected2"
+        multiple
+      ></Select>
+
+      <Select
+        class="w-full"
+        :options="[
+          {label: 'Last 7 days', value: '7', records: 110},
+          {label: 'Last 30 days', value: '30', records: 320},
+          {label: 'Last 90 days', value: '90', records: 310},
+          {label: 'None', value: null}
+        ]"
+        v-model="selected"
+      >
+        <template #item="{option}">
+          <div>
+            <span>{{ option.label }}</span>
+            <span class="ml-2 opacity-50">{{ option.records }} records</span>
+          </div>
+        </template>
+        <template #selected-item="{option}">
+          <span>{{ option.label }} 💫</span>
+        </template>
+      </Select>
+
+      <Select
+        class="w-full"
+        :options="[
+          {label: 'Last 7 days', value: '7'}, 
+          {label: 'Last 30 days', value: '30'}, 
+          {label: 'Last 90 days', value: '90'},
+        ]"
+        v-model="selected"
+      >
+        <template #extra-item>
+          <LinkButton to="/ranges">Manage ranges</LinkButton>
+        </template>
+
+      </Select>
+
+
+      <Input type="number" class="w-full">
+        <template #suffix>
+          USD
+        </template>
+      </Input>
+
+      <Tooltip>
+          <a :href="`https://google.com?q=adminforth`" target="_blank" >
+              <IconCardSearch class="w-5 h-5 me-2"/>
+          </a>
+
+          <template #tooltip>
+              Search for AdminForth
+          </template>
+      </Tooltip>
+
+      <VerticalTabs>
+        <template #tab:Profile>
+          <IconUserCircleSolid class="w-5 h-5 me-2"/>
+          Profile
+        </template>
+        <template #tab:Dashboard>
+          <IconGridSolid class="w-5 h-5 me-2"/>
+          Board
+        </template>
+        <template #Profile>
+          <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Profile Tab</h3>
+          <p class="mb-2">This is some placeholder content the Profile tab's associated content</p>
+        </template>
+        <template #Dashboard>
+          Dashboard Tab Content 
+        </template>
+      </VerticalTabs>
+
+      <Checkbox v-model="enable">
+        Enable
+      </Checkbox>
+
+      <Dialog class="w-96">
+        <template #trigger>
+          <Button>Dialog Toggle</Button>
+        </template>
+
+        <div class="space-y-4">
+          <p>This is the first paragraph of dialog content.</p>
+          <p>And this is the second paragraph.</p>
+        </div>
+      </Dialog>
+    </div>
+
+
+    <div class="flex flex-col gap-10 m-10 mt-20">
+
+      <Dialog
+        class="w-96"
+        header="Dialog Header"
+      >
+        <template #trigger>
+          <Button>Dialog Toggle</Button>
+        </template>
+
+        <div class="space-y-4">
+          <p>This is the first paragraph of dialog content.</p>
+          <p>And this is the second paragraph.</p>
+        </div>
+      </Dialog>
+
+      <Dropzone
+        :extensions="['.jpg', '.jpeg', '.png']"
+        :maxSizeBytes="1024 * 1024 * 2"
+        :multiple="false"
+        v-model="files"
+      />
+
+       <ProgressBar
+        :currentValue="2600"
+        :minValue="0"
+        :maxValue="5000"
+        />
+
+        <ProgressBar
+          :currentValue="1070"
+          :minValue="0"
+          :maxValue="5000"
+          :leftLabel="'Level 2'"
+          :rightLabel="'Level 3'"
+          :formatter="(value: number) => `${value} points`"
+          :progressFormatter="(value: number, percentage: number) => `${value} done`"
+        />
+
+        <div class="flex flex-col gap-2">
+          <Skeleton class="w-full h-4" />
+          <Skeleton class="w-full h-2" />
+          <Skeleton class="w-full h-2" />
+          <Skeleton class="w-full h-2" />
+          <Skeleton class="w-full h-2" />
+        </div>
+        
+        <Skeleton type="image" class="w-full h-full" />
+
+        <Skeleton type="video" class="w-full h-full" />
+
+        <Skeleton type="avatar" class="w-20 h-20" />
+
+        <Table
+          class="min-h-[300px]"
+          :columns="[
+            { label: 'Name', fieldName: 'name' },
+            { label: 'Age', fieldName: 'age' },
+            { label: 'Country', fieldName: 'country' },
+          ]"
+          :data="[
+            { name: 'John', age: 30, country: 'US' },
+            { name: 'Rick', age: 25, country: 'CA' },
+            { name: 'Alice', age: 35, country: 'UK' },
+            { name: 'Colin', age: 40, country: 'AU' },
+          ]"
+        ></Table>
+
+        <Spinner class="w-10 h-10" />
+
+
+    </div>
+
+
   </div>
 
 </template>
 
 <script setup lang="ts">
-import { ref, type Ref, onMounted, computed, watch } from 'vue';
-import dayjs from 'dayjs';
-import { callApi } from '@/utils';
-import { useI18n } from 'vue-i18n';
-import adminforth from '@/adminforth';
-import { AreaChart, BarChart, Input, Link, LinkButton, PieChart, Select, Table, VerticalTabs } from '@/afcl';
-import Button from '@/afcl/Button.vue';
-import Tooltip from '@/afcl/Tooltip.vue';
-import { IconUserCircleSolid, IconGridSolid } from '@iconify-prerendered/vue-flowbite';
+import { ref, watch } from 'vue';
 import Checkbox from '@/afcl/Checkbox.vue';
-const isoFlagToEmoji = (iso) => iso.toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397))
+import { Button } from '@/afcl'
+import { Link } from '@/afcl'
+import { LinkButton } from '@/afcl'
+import { Select } from '@/afcl'
+import { Input } from '@/afcl'
+import { Tooltip } from '@/afcl'
+import { VerticalTabs } from '@/afcl'
+import { IconGridSolid, IconUserCircleSolid } from '@iconify-prerendered/vue-flowbite';
+import { Dialog } from '@/afcl';
+import { Ref } from 'vue'
 import { Dropzone } from '@/afcl'
-// import Toggle from '@/afcl/Toggle.vue';
-// import BoolToggle from '@/controls/BoolToggle.vue'
+import { Table } from '@/afcl'
+import { ProgressBar } from '@/afcl';
+import { Skeleton } from '@/afcl';
+import { Spinner } from '@/afcl';
 
-function toggleSwitchHandler(value){
-  console.log("toggleSwitched: ",value);
-}
+
 
 const files: Ref<File[]> = ref([])
 
@@ -37,75 +236,14 @@ watch(files, (files) => {
     files.length = 0;
   }, 5000);
 })
+const enable = ref(false)
+const selected = ref(null)
+const selected2 = ref([])
 
-const data: Ref<{listedVsUnlistedPriceByDays: any, listedVsUnlistedByDays: any, 
-  apartsByDays: any, apartsCountsByRooms: any, topCountries: any, totalAparts: any} | null> = ref(null);
 
-const { t } = useI18n();
 
-const COLORS = ["#4E79A7", "#F28E2B", "#E15759", "#76B7B2", "#59A14F"]
+function doSmth(){
 
-const enable = ref(false);
+}
 
-const apartsCountsByDaysChart = computed(() => {
-  return data.value.apartsByDays?.reverse().map(
-    (item) => ({
-      x: dayjs(item.day).format('DD MMM'),
-      count: item.count
-    })
-  );
-});
-
-const listedVsUnlistedPriceByDays = computed(() => {
-  return data.value.listedVsUnlistedPriceByDays?.map(
-    (item) => ({
-      x: dayjs(item.day).format('DD MMM'),
-      listedPrice: item.listedPrice.toFixed(2),
-      unlistedPrice: item.unlistedPrice.toFixed(2),
-    })
-  );
-});
-
-const listedVsUnlistedCountByDays = computed(() => {
-  return data.value.listedVsUnlistedByDays?.map(
-    (item) => ({
-      x: dayjs(item.day).format('DD MMM'),
-      listed: item.listed,
-      unlisted: item.unlisted,
-    })
-  );
-});
-
-const apartsCountsByRooms = computed(() => {
-  return data.value.apartsCountsByRooms?.map(
-    (item, i) => ({
-      label: t(`{number_of_rooms} rooms`, { number_of_rooms: item.number_of_rooms }),
-      amount: item.count,
-      color: COLORS[i],
-    })
-  );
-});
-
-const topCountries = computed(() => {
-  return data.value.topCountries?.map(
-    (item, i) => ({
-      label: item.country,
-      amount: item.count,
-      color: COLORS[i],
-    })
-  );
-});
-
-onMounted(async () => {
-  // Fetch data from the API
-  try {
-    data.value = await callApi({path: '/api/dashboard/', method: 'GET'});
-  } catch (error) {
-    adminforth.alert({
-      message: `${t('Error fetching data: ')} ${error.message}`,
-      variant: 'danger',
-    });
-  }
-
-})
 </script>
