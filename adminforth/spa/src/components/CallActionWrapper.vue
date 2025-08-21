e65f705db8b4cc9bcafc19af08fe8c579c0c1c67
Maksym Pipkun <maxcim1077@gmail.com>
2025-08-21 10:25:11 +0300

    fix: enhance CallActionWrapper to accept extra props and update ThreeDotsMenu to handle null bulkActions

--- adminforth/spa/src/components/CallActionWrapper.vue
+++ adminforth/spa/src/components/CallActionWrapper.vue
@@ -5,12 +5,11 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ disabled?: boolean }>();
+const props = defineProps<{ disabled?: boolean, extra?: any }>();
 const emit = defineEmits<{ (e: 'callAction', extra?: any ): void }>();
 
 function onClick() {
     if (props.disabled) return;
-    const extra = { someData: 'example' };
-    emit('callAction', extra);
+    emit('callAction', props.extra);
 }
 </script>
