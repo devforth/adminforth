bf0fb48f9fa61e58c50588b166f777562b87809b
yaroslav8765 <pechorkin2014@gmail.com>
2025-09-15 13:47:26 +0300

    fix: update adminforth, inline-list and bulk-ai-flow plugins on live demo

--- live-demo/app/resources/apartments.ts
+++ live-demo/app/resources/apartments.ts
@@ -225,7 +225,7 @@ export default {
       },
       preview: {
         maxShowWidth: "480px",
-        //previewUrl: ({s3Path}) => `https://demo-static.adminforth.dev/${s3Path}`,
+        previewUrl: ({filePath}) => `https://demo-static.adminforth.dev/${filePath}`, 
       }
     }),
     new UploadPlugin({
@@ -244,6 +244,7 @@ export default {
       // You can use next to change preview URLs (if it is image) in list and show views
       preview: {
         maxShowWidth: "480px",
+        previewUrl: ({filePath}) => `https://demo-static.adminforth.dev/${filePath}`, 
       }
     }),
     new BulkAiFlowPlugin({
