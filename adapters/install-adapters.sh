dc37bd27ac74aaec684634787b5cb2a6b77b587a
yaroslav8765 <pechorkin2014@gmail.com>
2025-10-03 14:01:30 +0300

    feat: add login captcha and login captcha adapter to installation scripts

--- adapters/install-adapters.sh
+++ adapters/install-adapters.sh
@@ -3,7 +3,8 @@ ADAPTERS="adminforth-completion-adapter-open-ai-chat-gpt adminforth-email-adapte
 adminforth-email-adapter-mailgun adminforth-google-oauth-adapter adminforth-github-oauth-adapter \
 adminforth-facebook-oauth-adapter adminforth-keycloak-oauth-adapter adminforth-microsoft-oauth-adapter \ 
 adminforth-twitch-oauth-adapter adminforth-image-generation-adapter-openai adminforth-storage-adapter-amazon-s3 \ 
-adminforth-storage-adapter-local adminforth-image-vision-adapter-openai adminforth-key-value-adapter-ram"
+adminforth-storage-adapter-local adminforth-image-vision-adapter-openai adminforth-key-value-adapter-ram \
+adminforth-login-captcha-adapter-cloudflare"
 
 # for each
 install_adapter() {
