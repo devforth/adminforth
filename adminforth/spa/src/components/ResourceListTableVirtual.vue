7013a2e4765787d0e9fa1c59443a3afd502a595a
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2025-10-09 16:44:21 +0300

    fix: adjust table height for better layout consistency in ResourceListTableVirtual component

--- adminforth/spa/src/components/ResourceListTableVirtual.vue
+++ adminforth/spa/src/components/ResourceListTableVirtual.vue
@@ -14,7 +14,7 @@
             <div class="h-2 bg-lightListSkeletLoader rounded-full dark:bg-darkListSkeletLoader max-w-[360px]"></div>
         </div>      
     </div>
-    <table v-else class=" w-full text-sm text-left rtl:text-right text-lightListTableText dark:text-darkListTableText rounded-default">
+    <table v-else class="h-full w-full text-sm text-left rtl:text-right text-lightListTableText dark:text-darkListTableText rounded-default">
 
       <tbody>
         <!-- table header -->
@@ -74,7 +74,7 @@
           :column-widths="columnWidths"
         />
         
-        <tr v-else-if="rows.length === 0" class="bg-lightListTable dark:bg-darkListTable dark:border-darkListTableBorder">
+        <tr v-else-if="rows.length === 0" class="h-full bg-lightListTable dark:bg-darkListTable dark:border-darkListTableBorder">
           <td :colspan="resource?.columns.length + 2">
 
             <div id="toast-simple"
