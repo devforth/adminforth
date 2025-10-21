6c66d27875d70265e6428e4a7d4a4a3d5a04ab32
yaroslav8765 <pechorkin2014@gmail.com>
2025-10-21 13:25:28 +0300

    Revert "fix: use useI18n instead of window.i18n"
    This reverts commit 2a2b20bdf953672d1e44e7fccf92f8c357fdc42b.

--- adminforth/spa/src/main.ts
+++ adminforth/spa/src/main.ts
@@ -5,12 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { initI18n } from './i18n'
 
 export const app: ReturnType<typeof createApp> = createApp(App)
 /* IMPORTANT:ADMINFORTH COMPONENT REGISTRATIONS */
 
 app.use(createPinia())
 app.use(router)
+
+// get access to i18n instance outside components
+window.i18n = initI18n(app);
 
 
 /* IMPORTANT:ADMINFORTH CUSTOM USES */
