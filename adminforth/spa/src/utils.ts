adb40a8b11cde6be1b038dc5f43cbe75ee775711
yaroslav8765 <pechorkin2014@gmail.com>
2025-11-27 14:48:39 +0200

    feat: update vite version and resolve bug

--- adminforth/spa/src/utils.ts
+++ adminforth/spa/src/utils.ts
@@ -90,12 +90,12 @@ export const loadFile = (file: string) => {
   let baseUrl = '';
   if (file.startsWith('@/')) {
     path = file.replace('@/', '');
-    baseUrl = new URL(`./${path}`, import.meta.url).href;
+    baseUrl = new URL(`./${path}`, import.meta.url.split('?')[0]).href;
   } else if (file.startsWith('@@/')) {
     path = file.replace('@@/', '');
-    baseUrl = new URL(`./custom/${path}`, import.meta.url).href;
+    baseUrl = new URL(`./custom/${path}`, import.meta.url.split('?')[0]).href;
   } else {
-    baseUrl = new URL(`./${file}`, import.meta.url).href;
+    baseUrl = new URL(`./${file}`, import.meta.url.split('?')[0]).href;
   }
   return baseUrl;
 }
