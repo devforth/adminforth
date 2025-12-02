7363476ce94c8f6851af7d9d4ef614b7cf138b7d
tesaide <tesaideesasha@gmail.com>
2025-12-02 10:43:31 +0200

    fix: hide sidebar and center content on 404 page

--- adminforth/spa/src/views/PageNotFound.vue
+++ adminforth/spa/src/views/PageNotFound.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="absolute inset-0 flex flex-col items-center justify-center h-screen overflow-hidden">
+  <section class="flex flex-col items-center justify-center">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
-            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-blue-500">404</h1>
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-lightPrimary dark:text-darkPrimary">404</h1>
             <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">{{ $t("Something's missing.") }}</p>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{{ $t("Sorry, we can't find that page. You'll find lots to explore on the home page.") }} </p>
             <div class="flex justify-center">
