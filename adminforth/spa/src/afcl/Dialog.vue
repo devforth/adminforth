da033124e73673f95560143e2ef0dd2f7dfaf00d
yaroslav8765 <pechorkin2014@gmail.com>
2025-12-04 12:00:07 +0200

    feat: add askForCloseConfirmation prop to the afcl dialog

--- adminforth/spa/src/afcl/Dialog.vue
+++ adminforth/spa/src/afcl/Dialog.vue
@@ -22,7 +22,7 @@
               v-if="headerCloseButton"
               type="button"
               class="text-lightDialogCloseButton bg-transparent hover:bg-lightDialogCloseButtonHoverBackground hover:text-lightDialogCloseButtonHover rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:text-darkDialogCloseButton dark:hover:bg-darkDialogCloseButtonHoverBackground dark:hover:text-darkDialogCloseButtonHover"
-              @click="modal?.hide()"
+              @click="tryToHideModal"
             >
               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -51,13 +51,41 @@
           </div>
         </div>
       </div>
+      <div>
+        <!-- Confirmation Modal -->
+        <div
+          v-if="showConfirmationOnClose"
+          class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-60"
+        >
+          <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-sm w-full">
+            <h2 class="text-lg font-semibold mb-4 text-lightDialogHeaderText dark:text-darkDialogHeaderText">Confirm Close</h2>
+            <p class="mb-6 text-lightDialogBodyText dark:text-darkDialogBodyText">{{ props.closeConfirmationText }}</p>
+            <div class="flex justify-end">
+              <Button
+                class="me-3"
+                @click="showConfirmationOnClose = false"
+              >
+                Cancel
+              </Button>
+              <Button
+                @click="
+                  showConfirmationOnClose = false;
+                  modal?.hide();
+                "
+              >
+                Confirm
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Teleport>
 </template>
 
 <script setup lang="ts">
 import Button from "./Button.vue";
-import { ref, onMounted, nextTick, onUnmounted, type Ref } from 'vue';
+import { ref, onMounted, nextTick, onUnmounted, computed, type Ref } from 'vue';
 import { Modal } from 'flowbite';
 
 const modalEl = ref(null);
@@ -77,20 +105,42 @@ interface DialogProps {
   beforeCloseFunction?: (() => void | Promise<void>) | null
   beforeOpenFunction?: (() => void | Promise<void>) | null
   closable?: boolean
+  askForCloseConfirmation?: boolean
+  closeConfirmationText?: string
 }
 
 const props = withDefaults(defineProps<DialogProps>(), {
   header: '',
   headerCloseButton: true,
-  buttons: () => [
-    { label: 'Close', onclick: (dialog: any) => dialog.hide(), type: '' },
-  ],
+  buttons: () => [],
   clickToCloseOutside: true,
   beforeCloseFunction: null,
   beforeOpenFunction: null,
   closable: true,
+  askForCloseConfirmation: false,
+  closeConfirmationText: 'Are you sure you want to close this dialog?',
 })
 
+const buttons = computed<DialogButton[]>(() => {
+  if (props.buttons && props.buttons.length > 0) {
+    return props.buttons;
+  }
+  return [
+    {
+      label: 'Close',
+      onclick: (dialog: any) => {
+        if (!props.askForCloseConfirmation) {
+          dialog.hide();
+        } else {
+          showConfirmationOnClose.value = true;
+        }
+      },
+      options: {}
+    }
+  ];
+});
+
+const showConfirmationOnClose = ref(false);
 onMounted(async () => {
   //await one tick when all is mounted
   await nextTick();
@@ -130,5 +180,14 @@ defineExpose({
   open: open,
   close: close,
 })
+
+function tryToHideModal() {
+  if (!props.askForCloseConfirmation ) {
+    modal.value?.hide();
+  } else {
+    showConfirmationOnClose.value = true;
+  }
+}
+
 
 </script>
