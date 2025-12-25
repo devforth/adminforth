9448c2a2d4987eb2ea9b8020f61d83aaa383906f
yaroslav8765 <pechorkin2014@gmail.com>
2025-12-25 15:13:42 +0200

    fix: correct parameter name in modifyResourceConfig method for consistency

--- adminforth/basePlugin.ts
+++ adminforth/basePlugin.ts
@@ -36,7 +36,7 @@ export default class AdminForthPlugin implements IAdminForthPlugin {
     return 'non-uniquely-identified';
   }
 
-  modifyResourceConfig(adminforth: IAdminForth, resourceConfig: AdminForthResource, allPluginInstances?: {pi: AdminForthPlugin, resourceConfig: AdminForthResource}[]) {
+  modifyResourceConfig(adminforth: IAdminForth, resourceConfig: AdminForthResource, allPluginInstances?: {pi: AdminForthPlugin, resource: AdminForthResource}[]) {
     this.resourceConfig = resourceConfig;
     const uniqueness = this.instanceUniqueRepresentation(this.pluginOptions);
 
--- adminforth/types/Back.ts
+++ adminforth/types/Back.ts
@@ -455,7 +455,7 @@ export interface IAdminForthPlugin {
    * @param adminforth Instance of IAdminForth
    * @param resourceConfig Resource configuration object which will be modified by plugin
    */
-  modifyResourceConfig(adminforth: IAdminForth, resourceConfig: AdminForthResource, allPluginInstances?: {pi: IAdminForthPlugin, resourceConfig: AdminForthResource}[]): void;
+  modifyResourceConfig(adminforth: IAdminForth, resourceConfig: AdminForthResource, allPluginInstances?: {pi: IAdminForthPlugin, resource: AdminForthResource}[]): void;
   componentPath(componentFile: string): string;
 
   /**
