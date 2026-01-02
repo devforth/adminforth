3710c1fcb82ef028e22a1fe51d63d5fcb1467b2a
yaroslav8765 <pechorkin2014@gmail.com>
2026-01-02 14:07:58 +0200

    chore: add new resources and plugins to the dev demo[7]

---
+++ dev-demo/prisma/mysql/prisma.config.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  datasource: {
+    url: 'mysql://demo:demo@localhost:3307/demo',
+  },
+})
