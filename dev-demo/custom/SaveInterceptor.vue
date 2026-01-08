34cbcfd19b9976c66389e3e74451ef3632ed173e
Maksym Pipkun <maxcim1077@gmail.com>
2026-01-08 17:05:39 +0200

    fix: remove debug log from save interceptor

--- dev-demo/custom/SaveInterceptor.vue
+++ dev-demo/custom/SaveInterceptor.vue
@@ -4,7 +4,6 @@ import { useAdminforth } from "@/adminforth"
 const { registerSaveInterceptor } = useAdminforth()
 
 registerSaveInterceptor(async ({ action, values, resource }) => {
-  console.log('Custom Save Interceptor triggered:', { action, values, resource });
   const modal = window?.adminforthTwoFaModal;
   if (modal?.get2FaConfirmationResult) {
     const confirmationResult = await modal.get2FaConfirmationResult(
