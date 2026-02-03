9a4128926ec47335c199031df5692e880e0678ef
yaroslav8765 <pechorkin2014@gmail.com>
2026-02-03 17:09:29 +0200

    chore: update dev demo setup table names

--- dev-demo/migrations/prisma/mysql/prisma.config.ts
+++ dev-demo/migrations/prisma/mysql/prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   datasource: {
-    url: 'mysql://demo:demo@localhost:3307/demo',
+    url: 'mysql://demo:demo@localhost:3307/dev_demo',
   },
 })
