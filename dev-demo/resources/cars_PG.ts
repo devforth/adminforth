a6d50cb84d088631fbd18ee8936f713cdb8b7faa
yaroslav8765 <pechorkin2014@gmail.com>
2026-03-11 13:25:57 +0200

    dev-demo: update resources structure to don't use upload plugin arrays for clickhouse and use foreighn inline list only fot the cars_sqlite
    https://web.tracklify.com/project/2b7ZVgE5/AdminForth/1309/1ytAB7Tz/need-to-do-something-with-this

--- dev-demo/resources/cars_PG.ts
+++ dev-demo/resources/cars_PG.ts
@@ -1,4 +1,10 @@
 import carsResourseTemplate from "./carsResourseTemplate.js";
 
+const carsResource = carsResourseTemplate("cars_pg", "postgres", "id");
 
-export default carsResourseTemplate("cars_pg", "postgres", "id");
+export default {
+  ...carsResource,
+  plugins: [
+    ...(carsResource.plugins ?? []).filter(plugin => plugin.className !== "ForeignInlineListPlugin"),
+  ]
+};
