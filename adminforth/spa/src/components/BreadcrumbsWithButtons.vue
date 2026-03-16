08bd85882bcb8e5b71d7d9e2ea0d330207a3d769
Pavlo Kulyk <pkulyk@devforth.io>
2026-03-16 10:43:43 +0200

    fix: add check for correct error

--- adminforth/spa/src/components/BreadcrumbsWithButtons.vue
+++ adminforth/spa/src/components/BreadcrumbsWithButtons.vue
@@ -6,9 +6,15 @@
         <slot></slot>
       </div>
     </div>
-   
-    <div class="absolute inset-0 z-50 bg-white " v-if="coreStore.resourceColumnsError">
-      <PageNotFound/>
+    <div class="flex items-center justify-between mb-3 flex-wrap gap-y-2 gap-2" v-if="coreStore.resourceColumnsError">
+      <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <span class="font-medium">{{ $t('Error!') }}</span>
+        {{ coreStore.resourceColumnsError }}
+      </div>
+    </div>
+
+    <div class="absolute inset-0 z-50 bg-white " v-if="coreStore.resourceColumnsError?.includes('not found')">
+      <PageNotFound />
     </div>
   </div>
 
