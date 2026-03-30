aa8289bc5c6bb1f414167b08fc6a659ae8a1aad8
yaroslav8765 <pechorkin2014@gmail.com>
2026-03-30 08:18:39 +0300

    fix: add type-check for the spa ts errors

---
+++ live-demo/deploy/typeCheckSPA.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pnpm type-check 2>&1
+type_check_status=$?
+
+if [ $type_check_status -ne 0 ]; then
+  echo "Type check failed. Exiting with status code $type_check_status"
+  exit $type_check_status
+fi
