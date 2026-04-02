16859bc571be7ca80227a97db0eec908d04bb300
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-02 10:50:59 +0300

    fix: add spa-type-check logic to the adminforth build command

--- live-demo/deploy/typeCheckSPA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pnpx vue-tsc --build --force 2>&1
-type_check_status=$?
-
-if [ $type_check_status -ne 0 ]; then
-  echo "Type check failed. Exiting with status code $type_check_status"
-  exit $type_check_status
-fi
