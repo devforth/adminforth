58ba7a372a20187c1311643fa5bec6da47541cd0
yaroslav8765 <pechorkin2014@gmail.com>
2026-04-15 12:06:34 +0300

    dev-demo: add update_adminforth_plugins and add 'sessions/turns" resource

---
+++ dev-demo/resources/agent_resources/sessions.ts
@@ -0,0 +1,39 @@
+import AdminForth, { AdminForthDataTypes } from 'adminforth';
+import type { AdminForthResourceInput, AdminUser } from 'adminforth';
+import { randomUUID } from 'crypto';
+
+export default {
+  dataSource: 'sqlite',
+  table: 'sessions',
+  resourceId: 'sessions',
+  label: 'Sessions',
+  columns: [
+    {
+      name: 'id',
+      primaryKey: true,
+      type: AdminForthDataTypes.STRING,
+      fillOnCreate: ({ initialRecord, adminUser }) => randomUUID(),
+      showIn: {
+        edit: false,
+        create: false,
+      },
+    },
+    {
+      name: 'title',
+      type: AdminForthDataTypes.STRING,
+    },
+    {
+      name: 'turns',
+      type: AdminForthDataTypes.INTEGER,
+    },
+    {
+      name: 'asker_id',
+      type: AdminForthDataTypes.STRING,
+    },
+    {
+      name: 'created_at',
+      type: AdminForthDataTypes.DATE,
+      fillOnCreate: () => new Date(),
+    }
+  ],
+} as AdminForthResourceInput;
