015210ca0c74ef223229c6b6b33996e735199508
Pavlo Kulyk <pkulyk@devforth.io>
2026-05-01 10:53:58 +0300

    fix: enhance external link handling in Link component

--- adminforth/spa/src/afcl/Link.vue
+++ adminforth/spa/src/afcl/Link.vue
@@ -1,17 +1,35 @@
 <template>
+  <a 
+    v-if="isExternal"
+    v-bind="$attrs"
+    :href="to"
+    target="_blank"
+    rel="noopener noreferrer"
+    :class="linkClasses"
+  >
+    <slot></slot>
+  </a>
+
   <router-link 
+    v-else
     v-bind="$attrs"
     :to="to"
-    class="afcl-link text-lightPrimary underline dark:text-darkPrimary hover:no-underline hover:brightness-110
-      cursor-pointer"
+    :class="linkClasses"
   >
     <slot></slot>
   </router-link>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   to: string,
-}>()
+}>();
+
+const isExternal = computed(() => {
+  return typeof props.to === 'string' && props.to.startsWith('http');
+});
+
+const linkClasses = "afcl-link text-lightPrimary underline dark:text-darkPrimary hover:no-underline hover:brightness-110 cursor-pointer";
 </script>
