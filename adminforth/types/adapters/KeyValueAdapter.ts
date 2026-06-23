6ef7b51817a7ddb53d295f0cc1046e87bca0995f
yaroslav8765 <pechorkin2014@gmail.com>
2026-06-23 16:25:18 +0300

    feat: update key/value adapter interface by adding "collection" param
    AdminForth/1735/kv-adapters-and-storage-s3-com

--- adminforth/types/adapters/KeyValueAdapter.ts
+++ adminforth/types/adapters/KeyValueAdapter.ts
@@ -5,13 +5,13 @@
  */
 export interface KeyValueAdapter {
 
-  get(key: string): Promise<string | null>;
+  get(key: string, collection?: string): Promise<string | null>;
 
-  listByPrefix(prefix: string, limit: number): Promise<Record<string, string>[]>;  
+  listByPrefix(prefix: string, limit: number, collection?: string): Promise<Record<string, string>[]>;  
   
-  set(key: string, value: string, expiresInSeconds?: number): Promise<void>;
+  set(key: string, value: string, expiresInSeconds?: number, collection?: string): Promise<void>;
 
-  delete(key: string): Promise<void>;
+  delete(key: string, collection?: string): Promise<void>;
 
 }
 
