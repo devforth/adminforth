18e641aba54f4b91a7733ff83aa8ea47b91ca473
yaroslav8765 <pechorkin2014@gmail.com>
2026-06-26 13:39:55 +0300

    Revert "fix: unify dependency installation for npm and pnpm"
    This reverts commit 6f24eb17ecc4e667ff32b503a00ffc25697b351b.
    
    AdminForth/1704/create-json-editor-plugin

--- adminforth/commands/createApp/utils.js
+++ adminforth/commands/createApp/utils.js
@@ -487,13 +487,42 @@ async function writeTemplateFiles(dirname, cwd, useNpm, includePrismaMigrations,
   }
 }
 
-async function installDependencies(ctx, cwd, packageManager) {
-  const command = `${packageManager} install`;
+async function installDependenciesPnpm(ctx, cwd) {
+  const isWindows = process.platform === 'win32';
 
-  await Promise.all([
-    execAsync(command, { cwd, env: process.env }),
-    execAsync(command, { cwd: ctx.customDir, env: process.env }),
-  ]);
+  const nodeBinary = process.execPath; 
+  const pnpmPath = path.join(path.dirname(nodeBinary), isWindows ? 'pnpm.cmd' : 'pnpm');
+  const customDir = ctx.customDir;
+  if (isWindows) {
+    const res = await Promise.all([
+      execAsync(`pnpm install`, { cwd, env: { PATH: process.env.PATH } }),
+      execAsync(`pnpm install`, { cwd: customDir, env: { PATH: process.env.PATH } }),
+    ]);
+  } else {
+    const res = await Promise.all([
+      execAsync(`${nodeBinary} ${pnpmPath} install`, { cwd, env: { PATH: process.env.PATH } }),
+      execAsync(`${nodeBinary} ${pnpmPath} install`, { cwd: customDir, env: { PATH: process.env.PATH } }),
+    ]);
+  }
+}
+
+async function installDependenciesNpm(ctx, cwd) {
+  const isWindows = process.platform === 'win32';
+
+  const nodeBinary = process.execPath; 
+  const npmPath = path.join(path.dirname(nodeBinary), isWindows ? 'npm.cmd' : 'npm');
+  const customDir = ctx.customDir;
+  if (isWindows) {
+    const res = await Promise.all([
+      execAsync(`npm install`, { cwd, env: { PATH: process.env.PATH } }),
+      execAsync(`npm install`, { cwd: customDir, env: { PATH: process.env.PATH } }),
+    ]);
+  } else {
+    const res = await Promise.all([
+      execAsync(`${nodeBinary} ${npmPath} install`, { cwd, env: { PATH: process.env.PATH } }),
+      execAsync(`${nodeBinary} ${npmPath} install`, { cwd: customDir, env: { PATH: process.env.PATH } }),
+    ]);
+  }
 }
 
 function generateFinalInstructionsPnpm(skipPrismaSetup, options) {
@@ -567,7 +596,11 @@ export function prepareWorkflow(options) {
     {
       title: '📦 Installing dependencies...',
       task: async (ctx) => {
-        await installDependencies(ctx, ctx.projectDir, options.useNpm ? 'npm' : 'pnpm');
+        if (options.useNpm) {
+          await installDependenciesNpm(ctx, ctx.projectDir);
+        } else {
+          await installDependenciesPnpm(ctx, ctx.projectDir);
+        }
       }
     },
     {
